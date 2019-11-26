9f659b0e86047d666d8d174a9b5da2f328543b56
josomm22 <josommefeld@icloud.com>
2019-11-26 16:40:04 +0200

    basic game logic is ready

--- js/main.js
+++ js/main.js
@@ -1,47 +1,75 @@
-$(document).ready(function(){
-    $('.card').bind('click',flipCard)
+
+$(document).ready(function () {
+    $('.card').bind('click', flipCard)
     let pick1;
     let pick2;
     let attempts = 0;
+    const allCards = document.getElementsByClassName('card');
 
-    function flipCard(x){
+
+    function flipCard(x) {
         // console.log($(this).attr('id'));
-        if (pick1 === undefined){
-            $(this).removeClass('hidden');
-            pick1 = $(this);
-            console.log(`pick 1 is ${pick1}`)
-        }
-        else if (pick1 != undefined && pick2 === undefined){
-            $(this).removeClass('hidden');
-            pick2 = $(this);
-            console.log(`pick 2 is ${pick2}`);
-            setTimeout(checkCards,500);
-            // checkCards(pick1,pick2);
-
-
-        }else if (pick1 != undefined && pick2 != undefined){
-            console.log('naughty naughty');
+        
+        if ((this).classList.contains('hidden')){
+            if (pick1 === undefined) {
+                $(this).removeClass('hidden');
+                pick1 = $(this);
+                console.log(`pick 1 is ${pick1.attr('id')}`)
+            }
+            else if (pick1 != undefined && pick2 === undefined) {
+                $(this).removeClass('hidden');
+                pick2 = $(this);
+                console.log(`pick 2 is ${pick2.attr('id')}`);
+                setTimeout(checkCards, 500);
+                // checkCards(pick1,pick2);
+    
+    
+            } else if (pick1 != undefined && pick2 != undefined) {
+                console.log('naughty naughty');
+            }
+        }else{
+            console.log('this is already flipped');
         }
         
+
     };
-    function checkCards(){
-        if (pick1.attr('class') === pick2.attr('class')){
+
+    // This function checks if both card picks are the same
+    function checkCards() {
+        if (pick1.attr('class') === pick2.attr('class')) {
             console.log('well done');
             attempts += 1;
+            
+            if (gameWon()){
+                console.log('congratulations');
+
+            }else{
+                pick1 = undefined;
+                pick2 = undefined;
+            }
         }
-        else{
+        else {
             console.log('too bad');
-            attempts +=1;
-            flipBack(pick1,pick2);
+            attempts += 1;
+            flipBack(pick1, pick2);
+            pick1 = undefined;
+            pick2 = undefined;
         }
 
     };
-    function flipBack(pick1,pick2){
-        
+    function flipBack(pick1, pick2) {
+
         $(pick1).addClass('hidden');
         $(pick2).addClass('hidden');
-        pick1 = undefined;
-        pick2 = undefined;
+
+    };
+    function gameWon(){
+        let hiddenCards = document.getElementsByClassName('hidden');
+        if(hiddenCards.length === 0){
+            return true;
+        }else{
+            return false;
+        };
     };
 
 })
