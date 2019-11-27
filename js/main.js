586dac9da40db575c022c4c6ec1c61792f5ef289
josomm22 <josommefeld@icloud.com>
2019-11-27 16:27:33 +0200

    created start menu with images

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-
+ 
+function generateCards(){
+    let gameTypes = {easy: 12,medium:18, hard:24};
+    tempCardList = createCardList(gameTypes.medium);
+    
+    
+}
+function createCardList(amount){
+    let tempArr = [];
+    let i = 1;
+    while (tempArr.length <= amount/2){
+        tempArr.push(`card${i}`);
+        tempArr.push(`card${i}`);
+        i++;
+    }
+    return tempArr;
+}
 $(document).ready(function () {
     $('.card').bind('click', flipCard)
     const cardList =  ['card1','card1','card2','card2','card3','card3','card4','card4','card5','card5','card6','card6',];
@@ -19,6 +35,8 @@ $(document).ready(function () {
     };
     // remove commentout and remove the cardnumbers in the html
     // distributeCards(cardList);
+    
+
 
     function flipCard(x) {
         // console.log($(this).attr('id'));
