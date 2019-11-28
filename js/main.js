9fa10e74b2f33c44bea8818f67833197f1799fcf
josomm22 <josommefeld@icloud.com>
2019-11-28 11:59:07 +0200

    cards now use img tag and have flipping animation

--- js/main.js
+++ js/main.js
@@ -1,42 +1,58 @@
-$('.card-container').hide();
-let difficulty = 'easy';
-$('.diff').bind('click', function(){
-    difficulty = $(this).attr('id');
-    console.log(difficulty);
- });
+let defaultImg = ['./img/ca1.jpg', './img/ca2.jpg', './img/ca3.jpg', './img/ca4.jpg', './img/ca5.jpg', './img/ca6.jpg', './img/ca7.jpg', './img/ca8.jpg', './img/ca9.jpg', './img/ca10.jpg'];
 
-$('#start-btn').bind('click',startGame);
-function startGame(){
-    $('.start-menu').hide();
-    generateCards();
-    $('.card-container').show();
-
-}
-
-function generateCards(){
-    let gameTypes = {easy: 12,medium:18, hard:24};
-    tempCardList = createCardList(gameTypes[difficulty]);
-    console.log(tempCardList);
-    for (let i = 0; i < tempCardList.length; i++) {
-        
-        
-    }
-    
-    
-}
-function createCardList(amount){
-    let tempArr = [];
-    let i = 1;
-    while (tempArr.length < amount){
-        tempArr.push(`card${i}`);
-        tempArr.push(`card${i}`);
-        i++;
-    }
-    return tempArr;
-}
 $(document).ready(function () {
-    $('.card').bind('click', flipCard)
-    const cardList =  ['card1','card1','card2','card2','card3','card3','card4','card4','card5','card5','card6','card6',];
+    $('.card-container').hide();
+    let difficulty = 'easy';
+    $('.diff').on('click', function () {
+        difficulty = $(this).attr('id');
+        console.log(difficulty);
+    });
+
+    $('#start-btn').on('click', startGame);
+    function startGame() {
+        $('.start-menu').hide();
+        generateCards();
+        $('.card-container').show();
+        $('.card').on('click', flipCard)
+
+
+    }
+
+    function generateCards() {
+        let gameTypes = { easy: 12, medium: 18, hard: 24 };
+        tempCardList = createCardList(gameTypes[difficulty]);
+        console.log(tempCardList);
+        for (let i = 0; i < tempCardList.length; i++) {
+            cardOuter = $('<div/>').addClass(`card hidden ${tempCardList[i]}`).attr({ 'id': `card${i}` });
+            cardInner = $('<div/>').addClass(`flip-card-inner`);
+            cardFront = $('<div/>').addClass(`flip-card-front`);
+            cardBack = $('<div/>').addClass(`flip-card-back`);
+            imgFront = $('<img />').attr("src", `${defaultImg[tempCardList[i][4]]}`);
+            imgBack = $('<img />').attr("src", "./img/backcard.jpg");
+            $(cardBack).append(imgBack);
+            $(cardFront).append(imgFront);
+            $(cardInner).append(cardFront);
+            $(cardInner).append(cardBack);
+            $(cardOuter).append(cardInner);
+            // $card.css("background-image", `url()`
+            $('.card-container').append(cardOuter);
+        }
+
+
+        // <div class="card hidden card1" id="card1"></div>
+    }
+    function createCardList(amount) {
+        let tempArr = [];
+        let i = 0;
+        while (tempArr.length < amount) {
+            tempArr.push(`card${i}`);
+            tempArr.push(`card${i}`);
+            i++;
+        }
+        // return shuffle(tempArr); comment out if you want shuffle
+        return tempArr;
+    }
+    const cardList = ['card1', 'card1', 'card2', 'card2', 'card3', 'card3', 'card4', 'card4', 'card5', 'card5', 'card6', 'card6',];
 
     let pick1;
     let pick2;
@@ -45,16 +61,16 @@ $(document).ready(function () {
 
     const allCards = document.getElementsByClassName('card');
 
-    function distributeCards(cardArr){
-        let cardShuffle = shuffle(cardArr);
-        for (let i = 0; i < allCards.length; i++) {
-            $(allCards[i]).addClass(`${cardShuffle[i]}`);
-            
-        }
-    };
+    // function distributeCards(cardArr) {
+    //     let cardShuffle = shuffle(cardArr);
+    //     for (let i = 0; i < allCards.length; i++) {
+    //         $(allCards[i]).addClass(`${cardShuffle[i]}`);
+
+    //     }
+    // };
     // remove commentout and remove the cardnumbers in the html
     // distributeCards(cardList);
-    
+
 
 
     function flipCard(x) {
@@ -127,27 +143,27 @@ $(document).ready(function () {
         pick2 = undefined;
         for (let i = 0; i < allCards.length; i++) {
             $(allCards[i]).addClass('hidden');
-            
+
         }
-        
+
     }
     // this code comes from bost.ocks.org
     function shuffle(array) {
         var m = array.length, t, i;
-      
+
         // While there remain elements to shuffle…
         while (m) {
-      
-          // Pick a remaining element…
-          i = Math.floor(Math.random() * m--);
-      
-          // And swap it with the current element.
-          t = array[m];
-          array[m] = array[i];
-          array[i] = t;
+
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
         }
-      
+
         return array;
-      }
+    }
 
 })
